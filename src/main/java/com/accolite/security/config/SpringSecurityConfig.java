caa7ab0005a79fa1b8f10956d56d34c8549a0a08
Krishna Agarwal <krishna.agarwal@accolitedigital.com>
2021-01-11 17:10:13 +0530

    Review Comments

--- src/main/java/com/accolite/security/config/SpringSecurityConfig.java
+++ src/main/java/com/accolite/security/config/SpringSecurityConfig.java
@@ -11,8 +11,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers("/").permitAll();
 	}
 }
