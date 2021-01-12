fa508b809f2832d87b730cf62acacc71d48969ce
Krishna Agarwal <krishna.agarwal@accolitedigital.com>
2021-01-12 17:12:41 +0530

    Removing Extra Lines

--- src/main/java/com/accolite/security/config/SpringSecurityConfig.java
+++ src/main/java/com/accolite/security/config/SpringSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
