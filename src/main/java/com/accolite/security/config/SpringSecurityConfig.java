50b4e9d62d94a6aa5d1b7e5fbf68ea75e69914e7
Krishna Agarwal <krishna.agarwal@accolitedigital.com>
2021-01-13 18:10:02 +0530

    Code Updates

--- src/main/java/com/accolite/security/config/SpringSecurityConfig.java
+++ src/main/java/com/accolite/security/config/SpringSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.accolite.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -27,13 +30,22 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 			.logoutSuccessUrl("/login?logout")
 			.permitAll();
 	}
+	
+	/*
+	 * Update the below function to change the Password Encryption Scheme Replace
+	 * `getPasswordEncoder().encode("pass")` with `"{noop}pass"` to use No Password Encryption Scheme
+	 */
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// In-Memory Authentication
+		// In-Memory Authentication	
 		auth.inMemoryAuthentication()
-			.withUser("user").password("{noop}pass").roles("USER")
+			.withUser("user").password(getPasswordEncoder().encode("pass")).roles("USER")
 			.and()
-			.withUser("admin").password("{noop}pass").roles("ADMIN");
+			.withUser("admin").password(getPasswordEncoder().encode("pass")).roles("ADMIN");
 	}
 }
